0c889c1441025b7553c96e45b7128ff894a1b35d
carriexie939 <carriexie939@gmail.com>
2026-03-17 11:47:35 +1100

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE expense_tracker;
+
+USE expense_tracker;
+
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    type ENUM('income','expense') NOT NULL,
+    date DATE NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
