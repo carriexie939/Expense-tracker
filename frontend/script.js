f52be0ecce027d09c4765bdb3b0637c3dad139d0
carriexie939 <carriexie939@gmail.com>
2026-03-17 12:07:02 +1100

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,32 @@
+// when page loading
+document.addEventListener("DOMContentLoaded", function () {
+    loadTransactions();
+});
 
+// from the backend get the date
+function loadTransactions() {
+    fetch("http://localhost:8000/transactions")
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById("transaction-list");
+            list.innerHTML = "";
+
+            data.forEach(transaction => {
+                const row = document.createElement("tr");
+
+                row.innerHTML = `
+                    <td>${transaction.title}</td>
+                    <td>${transaction.category}</td>
+                    <td>${transaction.amount}</td>
+                    <td>${transaction.type}</td>
+                    <td>${transaction.date}</td>
+                    <td>
+                        <button onclick="deleteTransaction(${transaction.id})">Delete</button>
+                    </td>
+                `;
+
+                list.appendChild(row);
+            });
+        })
+        .catch(error => console.error("Error loading transactions:", error));
+}
